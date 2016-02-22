2f4433d8e2de6bce12e6be9a72f55554c13e01ee
TCarter16 <tcar0741@stu.raritanval.edu>
2016-02-22 09:33:07 -0500

    book.java

--- Book.java
+++ Book.java
@@ -9,18 +9,76 @@
 class Book
 {
     // The fields.
+    private int refNumber;
+    private int pages;
     private String author;
     private String title;
+    private String borrowed;
+    private boolean courseText;
 
-    /**
-     * Set the author and title fields when this object
-     * is constructed.
-     */
-    public Book(String bookAuthor, String bookTitle)
+
+    public Book(String bookAuthor, String bookTitle, String pages, String refNumber)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
+        borrowed = bookBorrowed;
+        courseText = bookCourseText;
     }
 
-    // Add the methods here ...
+   public String getAuthor()
+   {
+       return author;
+}
+
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public String getPages()
+    {
+        return pages;
+    }
+    
+    public void getRefNumber()
+    {
+        return refNumber;
+    }
+
+    public void printTitle()
+    {
+        System.out.println( title );
+    }
+    
+    public void printAuthor()
+    {
+        System.out.println( author );
+    }
+    
+    public void printPages()
+    {
+        System.out.println( pages );
+    }
+    
+    public void printRefNumber()
+    {
+        System.out.println(refNumber);
+    }
+    
+    public void setRefNumber(String ref)
+    {
+        refNumber = 103;
+    }
+    
+      public void setBorrowed(String ref)
+    {
+        if( borrowed == 0 && pages > 1)
+        ++borrowed;
+    }
+    public void printDetails()
+    {
+        System.out.println( "Title: " + printTitle + "/n Author: " + printAuthor + "/n Pages: " + printPages + "/n #: " + refNumber + "You've borrowed: " + borrowed);
+
+    }
 }
